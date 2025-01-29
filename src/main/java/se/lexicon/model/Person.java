39ea1dd733ac08bdcad0c13fda908046c8e91ffe
Loai Alwan <118984533+Meliods23@users.noreply.github.com>
2025-01-29 09:23:54 +0000

    Initiated Person Class

--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -5,4 +5,40 @@ package se.lexicon.model;
  */
 public class Person {
     // todo: needs completion
+
+    private int sequencer = 0;
+    private int id;
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName)
+    {
+        if(firstName != null)
+        {
+            this.firstName = firstName;
+        }
+        if(lastName != null)
+        {
+            this.lastName = lastName;
+        }
+    }
+
+    public int getNextId()
+    {
+        return 0;
+    }
+
+    public void loanBook()
+    {
+    }
+
+    public void returnBook()
+    {
+    }
+
+    public String getPersonInformation()
+    {
+        return null;
+    }
 }
